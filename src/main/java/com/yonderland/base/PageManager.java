115ec169dfff03e379a9b4806039e565af986b74
Fatih Ugur <fatih.ugur@appwise.be>
2022-08-31 12:20:54 +0200

    HelperMethods, PageManager, ConfigReader, BasePage, Constants created, java formuls added

--- src/main/java/com/yonderland/base/PageManager.java
+++ src/main/java/com/yonderland/base/PageManager.java
@@ -1,4 +1,12 @@
 package com.yonderland.base;
 
+import com.yonderland.pages.HomePage;
+
 public class PageManager {
+
+    public static HomePage homepage;
+
+    public static void initialize(){
+        homepage = new HomePage();
+    }
 }
--- src/main/java/com/yonderland/utils/Constants.java
+++ src/main/java/com/yonderland/utils/Constants.java
@@ -1,4 +1,11 @@
 package com.yonderland.utils;
 
 public class Constants {
+    public static final String CONFIGURATION_FILE = System.getProperty("user.dir") + "/src/test/resources/configuration/configuration.properties";
+
+    // used to tell the web driver to wait for a certain amount of time before it throws a 'no such element exception'
+    // here driver wait for the element for that time before throwing an exception
+    public static final int IMPLICIT_WAIT_TIME = 10;
+    //
+    public static final int EXPLICIT_WAIT_TIME = 15;
 }
